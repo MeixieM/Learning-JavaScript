3a6b8a5faff80840c07a7d0497830513fcdfd4ce
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-21 04:11:14 +0800

    finished save btn

---
+++ JS Challenges/Challenge#7.js
@@ -0,0 +1,8 @@
+// Create a variable, message, that stores the string: "You have tree new notifications"
+ let username = "Alex"
+let message = "You have three new notifications"
+
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
+
+// Create a variable, messageToUser, that contains the message we have logged
--- Passenger-Counter-App/index.js
+++ Passenger-Counter-App/index.js
@@ -1,15 +1,21 @@
 // grab the count-el element, store it in a countEl variable
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     // set countEl's innerText to the count
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
-
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+   // 3. Render the variable in the saveEl using innerText
+   // NB: Make sure to not delete the existing content of the paragraph
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
+
