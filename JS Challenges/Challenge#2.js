a269fab98df9669e76c5c12fb8cc6d4b9743df60
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-20 22:49:14 +0800

    Added JS Challenges Folder

---
+++ JS Challenges/Challenge#2.js
@@ -0,0 +1,10 @@
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 21
+let humanDogRatio = 7
+
+// 2. Multiply the two together and store the result in myDogAge 
+myDogAge = myAge * humanDogRatio
+
+// 3. Log myDogAge to the console
+console.log(myDogAge)
--- Passenger-Counter-App/index.js
+++ Passenger-Counter-App/index.js
@@ -1,1 +1,26 @@
-document.getElementById('count-el').innerText = 5;
+// document.getElementById('count-el').innerText = 5;
+
+// let count = 0
+
+// console.log(count)
+
+// Challege #1
+// 1. Create a variable, myAge, and set its value to your age
+// let myAge = 21
+
+// 2. Log the myAge variable to the console
+// console.log(myAge)
+
+//Challeng #2
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 21
+let humanDogRatio = 7
+
+// 2. Multiply the two together and store the result in myDogAge 
+myDogAge = myAge * humanDogRatio
+
+// 3. Log myDogAge to the console
+console.log(myDogAge)
+
+
