6283bda22565aa99d89d36f4dcbbb2578124e6af
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-21 00:11:43 +0800

    added increment btn

---
+++ JS Challenges/Challenge#3.js
@@ -0,0 +1,13 @@
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoint = 50
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint + 50
+console.log(bonusPoint)
+// Decrease it down to 25, and then finally increase it to 70
+bonusPoint = bonusPoint - 75
+console.log(bonusPoint)
+bonusPoint = bonusPoint + 45
+
+// Console.log the value after each step
+console.log(bonusPoint)
---
+++ JS Challenges/Challenge#4.js
@@ -0,0 +1,6 @@
+// Create a function (you decide the name) that logs out the number 42 to the console
+function myNumber() {
+    console.log(42);
+}
+// Call/invoke the function
+myNumber()
