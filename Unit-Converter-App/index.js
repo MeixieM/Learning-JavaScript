c52ed7458d5cf395af34caf6a882b56d78ac8d7f
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-22 11:49:47 +0800

    round to 3 decimals

--- Unit-Converter-App/index.js
+++ Unit-Converter-App/index.js
@@ -5,9 +5,9 @@ let conversionInput1 = document.getElementById("conversion1")
 
 
 function length() {
-    let meter = num1 * 3.28084
+    let meter = num1 * 3.28084 
     let feet = num1 * 0.3048 
-    conversionInput1.textContent = num1 + "meters ="+ meter +  "feet |" +num1+ "feet ="+  feet+"meters"
+    conversionInput1.textContent = num1 + " meters = "+ meter.toFixed(3) +  " feet | " + num1 + " feet = "+  feet +" meters"
 }
 
 
