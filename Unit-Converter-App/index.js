160016638f4f2b650deda4a7642e5592e03b5862
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-22 11:47:12 +0800

    Added Unit Conversion Folder

---
+++ Unit-Converter-App/index.js
@@ -0,0 +1,14 @@
+let num1 = 20
+document.getElementById("num-enter").textContent = num1
+
+let conversionInput1 = document.getElementById("conversion1")
+
+
+function length() {
+    let meter = num1 * 3.28084
+    let feet = num1 * 0.3048 
+    conversionInput1.textContent = num1 + "meters ="+ meter +  "feet |" +num1+ "feet ="+  feet+"meters"
+}
+
+
+length()
