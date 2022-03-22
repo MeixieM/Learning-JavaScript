37699d733bc1df4adc9e9574f88b2c7ebc831321
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-23 06:05:52 +0800

    finish the app

--- Unit-Converter-App/index.js
+++ Unit-Converter-App/index.js
@@ -1,14 +1,25 @@
-let num1 = 20
-document.getElementById("num-enter").textContent = num1
+
 
 let conversionInput1 = document.getElementById("conversion1")
+let conversionInput2 = document.getElementById("conversion2")
+let conversionInput3 = document.getElementById("conversion3")
 
+document.getElementById("myButton").onclick = function () {
+    var inputNum = document.getElementById("num-enter").value;
+    
+        let meter = inputNum * 3.28084 
+        let feet = inputNum * 0.3048 
+        conversionInput1.textContent = inputNum + " meters = "+ meter.toFixed(3) +  " feet | " + inputNum + " feet = "+  feet.toFixed(3) +" meters"
+  
+        let liters = inputNum * 3.785411784
+        let gallons = inputNum * 0.2641720524 
+        conversionInput2.textContent = inputNum + " liters = "+ gallons.toFixed(3) +  " gallons | " + inputNum + " gallons = "+  liters.toFixed(3) +" liters"
+    
 
-function length() {
-    let meter = num1 * 3.28084 
-    let feet = num1 * 0.3048 
-    conversionInput1.textContent = num1 + " meters = "+ meter.toFixed(3) +  " feet | " + num1 + " feet = "+  feet +" meters"
+        let pounds = inputNum * 0.45359237
+        let kilos = inputNum / 0.45359237
+        conversionInput3.textContent = inputNum + " kilos = "+ kilos.toFixed(3) +  " pounds | " + inputNum + " pounds = "+  pounds.toFixed(3) +" kilos"
 }
 
 
-length()
+
