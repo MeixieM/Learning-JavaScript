c29fd419c9d126f06960c1761da9a917ff3e23c8
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-25 10:34:54 +0800

    added challenge#10

---
+++ JS Challenges/Challenge#10.js
@@ -0,0 +1,13 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 2
+let secondCard = 3
+
+let sum = firstCard + secondCard
+
+console.log(sum)
+ 
+ 
